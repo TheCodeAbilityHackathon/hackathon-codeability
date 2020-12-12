84990a12c2d1fba8a51d765b615d1e8db532024c
maciejmatu <maciek.matuszewski@gmail.com>
2020-12-13 00:46:24 +0100

    Remove formik

--- src/App.tsx
+++ src/App.tsx
@@ -23,14 +23,6 @@ import {
   list,
 } from "@chakra-ui/react";
 import theme from "./theme";
-import {
-  Formik,
-  Form,
-  Field,
-  FieldProps,
-  useField,
-  FieldAttributes,
-} from "formik";
 import { useState } from "react";
 import { getFlatOptions, getNextBranchId } from "./utils";
 import disabilities from "./data/disabilities.json";
@@ -41,39 +33,45 @@ const ids = flatOptions.map(({ value }) => value);
 const BooleanQuestionField = ({
   label,
   name,
-  onChange,
+  onClick,
+  renderLabel,
 }: {
   label: string;
   name: string;
-  onChange(value: "yes" | "no"): void;
+  onClick(value: boolean): void;
+  renderLabel?(text: string): React.ReactNode;
 }) => {
-  const [field, meta, helpers] = useField<"yes" | "no">(name);
-  console.log(field);
-  console.log(field.value);
-
   return (
-    <FormControl as="fieldset">
-      <FormLabel htmlFor={field.name} as="legend">
-        {label}
+    <VStack spacing={8}>
+      <FormLabel as="legend">
+        {renderLabel ? renderLabel(label) : label}
       </FormLabel>
-      <RadioGroup
-        {...field}
-        onChange={(value: "yes" | "no") => {
-          onChange && onChange(value);
-          helpers.setValue(value);
-        }}
-        id={field.name}
-      >
-        <HStack spacing="24px">
-          <Radio value="yes">Tak</Radio>
-          <Radio value="no">Nie</Radio>
-        </HStack>
-      </RadioGroup>
-    </FormControl>
+      <HStack spacing="24px">
+        <Button
+          onClick={() => onClick(true)}
+          type="button"
+          borderRadius={50}
+          colorScheme="pink"
+          size="lg"
+        >
+          Tak
+        </Button>
+        <Button
+          onClick={() => onClick(false)}
+          type="button"
+          borderRadius={50}
+          colorScheme="pink"
+          size="lg"
+        >
+          Nie
+        </Button>
+      </HStack>
+    </VStack>
   );
 };
 
 export const App = () => {
+  const [disabilities, setDisabilities] = useState<string[]>([]);
   const [currentId, setCurrentId] = useState<string>(ids[0]);
   const [finishedStep, setFinishedStep] = useState<boolean>(false);
   const currentOption = flatOptions.find(({ value }) => value === currentId);
@@ -94,69 +92,35 @@ export const App = () => {
               </Text>
             </VStack>
 
-            <VStack mt="8" spacing={16}>
-              <VStack spacing={4} mb={8}>
-                <Formik
-                  initialValues={{}}
-                  onSubmit={(values, actions) => {
-                    console.log("submit", values);
-                  }}
-                >
-                  <Form>
-                    {currentOption && (
-                      <BooleanQuestionField
-                        name={currentOption.value}
-                        label={currentOption.label}
-                        onChange={(value) => {
-                          const nextId = getNextBranchId(
-                            value !== "yes",
-                            ids,
-                            currentId
-                          );
+            {!finishedStep ? (
+              <VStack mt="8" spacing={16}>
+                <VStack spacing={4} mb={8}>
+                  {currentOption && (
+                    <BooleanQuestionField
+                      name={currentOption.value}
+                      label={currentOption.label}
+                      onClick={(value) => {
+                        const nextId = getNextBranchId(value, ids, currentId);
+                        if (value) {
+                          setDisabilities([
+                            ...disabilities,
+                            currentOption.value,
+                          ]);
+                        }
 
-                          if (!nextId) {
-                            setFinishedStep(true);
-                          } else {
-                            setCurrentId(nextId);
-                          }
-                        }}
-                      />
-                    )}
-                    {/* <Field name={multistepForm[index].value}>
-                      {({ field, form }: FieldProps) => (
-                        
-                        <Button type="button" onClick={() => {}}>Yes</Button>
-                        <Button type="button" onClick={() => {}}>No</Button>
-                        <FormControl
-                          isInvalid={Boolean(
-                            form.errors.name && form.touched.name
-                          )}
-                        >
-                          <FormLabel htmlFor={field.name}>First name</FormLabel>
-                          <Input
-                            {...field}
-                            value={field.value || ""}
-                            id={field.name}
-                            placeholder="Name..."
-                          />
-                          <FormErrorMessage>
-                            {form.errors.name}
-                          </FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field> */}
-                    <Button
-                      type="submit"
-                      borderRadius={50}
-                      colorScheme="pink"
-                      size="lg"
-                    >
-                      SUBMIT
-                    </Button>
-                  </Form>
-                </Formik>
+                        if (!nextId) {
+                          setFinishedStep(true);
+                        } else {
+                          setCurrentId(nextId);
+                        }
+                      }}
+                    />
+                  )}
+                </VStack>
               </VStack>
-            </VStack>
+            ) : (
+              <Text>Wartości: {disabilities.join(" ")}</Text>
+            )}
           </Container>
         </Grid>
       </Box>

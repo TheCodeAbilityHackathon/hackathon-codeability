ad60b62eefa416812cc87d44539866057f679e38
maciejmatu <maciek.matuszewski@gmail.com>
2020-12-13 07:14:57 +0100

    Remove ununsed imports

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Center } from "@chakra-ui/react";
+import { Box, BoxProps } from "@chakra-ui/react";
 import React from "react";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 
